a1f21e9990b6da13ca7f926097bf8921a2f90e0d
Souvik <shadyxv123445@gmail.com>
2024-02-08 00:24:22 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+	import FeedbackForm from './components/FeedbackForm.svelte'
+	import FeedbackList from './components/FeedbackList.svelte'
+	import FeedbackStats from './components/FeedbackStats.svelte'
+</script>
+<head> <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Teko:wght@300;400;500;600;700&display=swap"></head>
+<style>
+	a{
+		color: red;
+		font-family: 'Teko', sans-serif;
+		font-weight: 900;
+	}
+</style>
+
+<main class="container">
+	<FeedbackForm />
+	<FeedbackStats />
+	<FeedbackList />
+
+</main>
+<footer class="container">
+		<p>Made with <a  href="https://svelte.dev/tutorial">Svelte</a></p>
+</footer>
